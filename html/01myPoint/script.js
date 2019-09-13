c56a2e0ceadc80c8cdf67ce0c47fb64b2db4d2ea
Jorikos <26204@ma-web.nl>
2019-09-13 12:23:12 +0200

    wtf

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,28 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let points = [];
+
+function animate(){
+  context.clearRect(0,0,width,height)
+  requestAnimationFrame(animate);
+
+  let A = new Point(new Vector2d(getRandom(width),getRandom(height)),10);
+  points.push(A);
+  //A.draw(context);
+
+  for(let i = 0; i<points.length;i++){
+    points[i].radius += 1;
+    points[i].draw(context)
+    if(points[i].radius > 50){
+      points.splice(i,1);
+    }
+  }
+}
+
+animate();
+
+function getRandom(max){
+  return Math.floor(Math.random()*max);
+}
